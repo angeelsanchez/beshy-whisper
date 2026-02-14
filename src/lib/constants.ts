9b03e7ad1318607b097bb38c04a97c5660aa8d27
Angel Sanchez <angelsanchezdev@gmail.com>
2026-02-14 16:30:00 +0100

    refactor: extract reminder time constants, eliminate magic numbers

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,8 @@
 export const UUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
+
+export const MORNING_REMINDER_START = 600;
+export const MORNING_REMINDER_END = 630;
+export const STREAK_WARNING_START = 900;
+export const STREAK_WARNING_END = 1081;
+export const NIGHT_REMINDER_START = 1290;
+export const NIGHT_REMINDER_END = 1320;
